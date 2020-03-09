c20cde56da63ae0aaf44e5f2363fb85d5221ab43
Amanda Hogan <ahogan@teamworks.com>
2020-03-09 13:22:35 -0400

    Move port variable to match with server.listen

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 const express = require('express');
-const db = require('./data/db');
 
 const server = express();
 
-const PORT = 5000;
-
 server.post('/api/users', (req, res) => {
   res.status(400).json({ errorMessage: "Please provide name and bio for the user." }); // If name or bio missing from req body
-  res.status(201).db.find(); // Send back created user
+  res.status(201); // Send back created user
   res.status(500).json({ errorMessage: "There was an error while saving the user to the database" });
 });
 
@@ -31,5 +28,7 @@ server.patch('/api/users/:id', (req, res) => {
   res.status(500).json({ errorMessage: "The user information could not be modified." });
   res.status(200).json(); // Send back updated user
 });
+
+const PORT = 5000;
 
 server.listen(PORT, () => console.log(`\n ** API listening on http://localhost:${PORT} **\n`));
