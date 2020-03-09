cdb14a634d156aebeae0a5ae615a2d6873dfe2a7
Amanda Hogan <ahogan@teamworks.com>
2020-03-09 13:18:53 -0400

    Setup dependencies and created server

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const db = require('./data/db');
+
+const server = express();
+
+const PORT = 5000;
+
+server.post('/api/users', (req, res) => {
+  res.status(400).json({ errorMessage: "Please provide name and bio for the user." }); // If name or bio missing from req body
+  res.status(201).db.find(); // Send back created user
+  res.status(500).json({ errorMessage: "There was an error while saving the user to the database" });
+});
+
+server.get('/api/users', (req, res) => {
+  res.status(500).json({ errorMessage: "The users information could not be retrieved." });
+});
+
+server.get('/api/users/:id', (req, res) => {
+  res.status(404).json({ message: "The user with the specified ID does not exist." }); // If id in req body not found
+  res.status(500).json({ errorMessage: "The user information could not be retrieved." });
+});
+
+server.delete('/api/users/:id', (req, res) => {
+  res.status(404).json({ message: "The user with the specified ID does not exist." }); // If id in req body not found
+  res.status(500).json({ errorMessage: "The user could not be removed" });
+});
+
+server.put('/api/users/:id', (req, res) => {
+  res.status(404).json({ message: "The user with the specified ID does not exist." }); // If id in req body not found
+  res.status(400).json({ errorMessage: "Please provide name and bio for the user." }); // If name or bio missing from req body
+  res.status(500).json({ errorMessage: "The user information could not be modified." });
+  res.status(200).json(); // Send back updated user
+});
+
+server.listen(PORT, () => console.log(`\n ** API listening on http://localhost:${PORT} **\n`));
